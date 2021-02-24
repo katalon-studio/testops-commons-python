bab515e66f3271c917ae8db51a4b268fe41ad372
sonhuynhpham <sonhuynhpham@kms-technology.com>
2021-02-24 11:20:31 +0700

    Handle file upload

--- testops_commons/katalon/testops/commons/testops_connector.py
+++ testops_commons/katalon/testops/commons/testops_connector.py
@@ -1,33 +1,22 @@
-import os
-
-import urllib3
+import requests
 from testops_api import ApiClient
 from testops_api.api.file_api import FileApi
 from testops_api.api.test_report_api import TestReportApi
-from testops_api.model.file_resource import FileResource
-from testops_api.model.upload_batch_file_resource import UploadBatchFileResource
-from urllib3 import PoolManager
 
 
 class TestOpsConnector:
-    api_client: ApiClient
 
     def __init__(self, api_client: ApiClient):
         self.api_client = api_client
 
     def get_upload_urls(self, project_id: int, num_of_urls: int) -> list:
-        fa: FileApi = FileApi(self.api_client)
-        # print(project_id, number_of_)
+        fa = FileApi(self.api_client)
         return fa.get_upload_urls(project_id, num_of_urls)
 
     def upload_testops_report(self, body: list, project_id: int, batch: str) -> None:
-        api: TestReportApi = TestReportApi(self.api_client)
+        api = TestReportApi(self.api_client)
         api.process_test_ops_reports(project_id, batch, body)
 
     def upload_file(self, url: str, file: str):
         with open(file, 'rb') as f:
-            http: PoolManager = PoolManager()
-            http.request('PUT', url, {
-                'filefield': (os.path.basename(file), f.read(), 'application/json'),
-            })
-
+            return requests.put(url, data=f)
