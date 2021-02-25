430a6db1ac5d89ad4adc3196da585d0e59f01726
sonhuynhpham <sonhuynhpham@kms-technology.com>
2021-02-25 17:11:04 +0700

    Reorganize project tree

--- testops_commons/configuration/configuration.py
+++ testops_commons/configuration/configuration.py
@@ -1,55 +1,116 @@
 from pathlib import Path
 
 from testops_commons.core.constants import *
-from testops_commons.helper.helper import (
-    ConfigurationHelper, is_blank)
+from testops_commons.helper.helper import ConfigurationHelper, is_blank
+
+
+class ProxyInformation:
+    def __init__(self,
+                 host: str,
+                 port: int,
+                 username: str,
+                 password: str,
+                 protocol: str):
+        self.host = host
+        self.port = port
+        self.username = username
+        self.password = password
+        self.protocol = protocol
 
 
 class Configuration:
-    def __init__(self, server_url, api_key, project_id, report_folder, build_label, build_url, ):
+    def __init__(self,
+                 server_url: str,
+                 api_key: str,
+                 project_id: int,
+                 report_folder: str,
+                 build_label: str,
+                 build_url: str,
+                 proxy_information: ProxyInformation):
         self.server_url = server_url
         self.api_key = api_key
         self.project_id = project_id
         self.report_folder = report_folder
         self.build_url = build_url
         self.build_label = build_label
+        self.proxy_information = proxy_information
 
 
-class ConfigurationCreator:
+class TestOpsConfigurationCreator:
+
+    def __init__(self):
+        self.conf = ConfigurationHelper()
+
+    def create_proxy_information(self) -> ProxyInformation:
+        host = self.conf.get_config(
+            TESTOPS_PROXY_HOST,
+            env_name=TESTOPS_PROXY_HOST_ENV)
+
+        port_str = self.conf.get_config(
+            TESTOPS_PROXY_PORT,
+            env_name=TESTOPS_PROXY_PORT_ENV)
+        port = -1
+        if not is_blank(port_str):
+            port = int(port_str)
+
+        username = self.conf.get_config(
+            TESTOPS_PROXY_USERNAME,
+            env_name=TESTOPS_PROXY_USERNAME_ENV,
+            default='')
+
+        password = self.conf.get_config(
+            TESTOPS_PROXY_PASSWORD,
+            env_name=TESTOPS_PROXY_PASSWORD_ENV,
+            default='')
+
+        protocol = self.conf.get_config(
+            TESTOPS_PROXY_SERVER_TYPE,
+            env_name=TESTOPS_PROXY_SERVER_TYPE_ENV,
+            default=TESTOPS_PROXY_SERVER_TYPE_DEFAULT)
+
+        return ProxyInformation(host=host,
+                                port=port,
+                                username=username,
+                                password=password,
+                                protocol=protocol)
 
     def create_configuration(self) -> Configuration:
-        pass
-
-
-class TestOpsConfigurationCreator(ConfigurationCreator):
-
-    def create_configuration(self) -> Configuration:
-        config = ConfigurationHelper()
-        server_url: str = config.get_config(
+        server_url = self.conf.get_config(
             TESTOPS_SERVER_URL,
             env_name=TESTOPS_SERVER_URL_ENV,
             default=TESTOPS_SERVER_URL_DEFAULT)
 
-        report_folder_path: str = config.get_config(
+        project_id_str = self.conf.get_config(
+            TESTOPS_PROJECT_ID,
+            env_name=TESTOPS_PROJECT_ID_ENV)
+        project_id = -1
+        if not is_blank(project_id_str):
+            project_id = int(project_id_str)
+
+        report_folder_path = self.conf.get_config(
             TESTOPS_REPORT_FOLDER,
             env_name=TESTOPS_REPORT_FOLDER_ENV,
             default=TESTOPS_REPORT_FOLDER_DEFAULT)
         report_folder: Path = Path(report_folder_path)
 
-        api_key: str = config.get_config(
+        api_key = self.conf.get_config(
             TESTOPS_API_KEY,
             env_name=TESTOPS_API_KEY_ENV)
-        build_label: str = config.get_config(
+
+        build_label = self.conf.get_config(
             TESTOPS_BUILD_LABEL,
             env_name=TESTOPS_BUILD_LABEL_ENV)
-        build_url: str = config.get_config(
+
+        build_url = self.conf.get_config(
             TESTOPS_BUILD_URL,
             env_name=TESTOPS_BUILD_URL_ENV)
-        pj_id_str: str = config.get_config(
-            TESTOPS_PROJECT_ID,
-            env_name=TESTOPS_PROJECT_ID_ENV)
-        project_id: int = -1
-        if not is_blank(pj_id_str):
-            project_id = int(pj_id_str)
 
-        return Configuration(server_url, api_key, project_id, report_folder, build_label, build_url)
+        proxy_information = self.create_proxy_information()
+
+        return Configuration(server_url=server_url,
+                             api_key=api_key,
+                             project_id=project_id,
+                             report_folder=report_folder,
+                             build_label=build_label,
+                             build_url=build_url,
+                             proxy_information=proxy_information)
