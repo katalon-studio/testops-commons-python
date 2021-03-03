507e142e0078669e29300c95ae800a4231f79531
Giang Tong <giang.tong@katalon.com>
2021-03-03 17:08:15 +0700

    fix proxy

--- testops_commons/uploader/uploader.py
+++ testops_commons/uploader/uploader.py
@@ -37,16 +37,20 @@ class TestOpsReportUploader(ReportUploader):
             password=self.configuration.api_key
         )
         config.verify_ssl = False
-        if self.configuration.proxy_information.protocol == PROXY_PROTOCOL_HTTP:
-            config.proxy = HTTPConnectionPool(host=self.configuration.proxy_information.host
-                                              , port=self.configuration.proxy_information.port)
-        else:
-            config.proxy = HTTPSConnectionPool(host=self.configuration.proxy_information.host
-                                               , port=self.configuration.proxy_information.port)
-        if self.configuration.proxy_information.username and self.configuration.proxy_information.password:
-            config.proxy_headers = make_headers(proxy_basic_auth="{}:{}"
-                                                .format(self.configuration.proxy_information.username,
-                                                        self.configuration.proxy_information.password))
+
+        if self.configuration.proxy_information.host:
+            if self.configuration.proxy_information.protocol == PROXY_PROTOCOL_HTTP:
+                config.proxy = HTTPConnectionPool(host=self.configuration.proxy_information.host
+                                                  , port=self.configuration.proxy_information.port)
+            elif self.configuration.proxy_information.protocol == PROXY_PROTOCOL_HTTPS:
+                config.proxy = HTTPSConnectionPool(host=self.configuration.proxy_information.host
+                                                   , port=self.configuration.proxy_information.port)
+            proxy_user = self.configuration.proxy_information.username
+            proxy_pass = self.configuration.proxy_information.password
+            if (proxy_user is not None) and (proxy_pass is not None):
+                config.proxy_headers = make_headers(proxy_basic_auth="{}:{}"
+                                                    .format(self.configuration.proxy_information.username,
+                                                            self.configuration.proxy_information.password))
 
         client: ApiClient = ApiClient(config)
         return client
