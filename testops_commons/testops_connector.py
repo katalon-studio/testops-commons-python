ac2510dc498dd395b2b607948c19e7a40df5616e
Nguyen Tri Nhan <nguyentrinhan.dev@gmail.com>
2022-11-30 16:37:12 +0700

    refactor: use testops_api_python library to call api

--- testops_commons/testops_connector.py
+++ testops_commons/testops_connector.py
@@ -2,7 +2,12 @@ import requests
 from testops_api import ApiClient
 from testops_api.api.file_api import FileApi
 from testops_api.api.test_report_api import TestReportApi
-
+from testops_api.api.visual_testing_api import VisualTestingApi
+from testops_api.model.checkpoint_resource import CheckpointResource
+from testops_api.model.search_request import SearchRequest
+from testops_api.model.search_request_condition import SearchRequestCondition
+from testops_api.model.search_request_pagination import SearchRequestPagination
+from testops_api.api.search_api import SearchApi
 
 class TestOpsConnector:
 
@@ -20,3 +25,38 @@ class TestOpsConnector:
     def upload_file(self, url: str, file: str):
         with open(file, 'rb') as f:
             return requests.put(url, data=f)
+
+    def get_upload_url(self, project_id: int):
+        api = FileApi(self.api_client)
+        return api.get_upload_url(project_id)
+
+    def upload_checkpoint(self, body) -> CheckpointResource:
+        api = VisualTestingApi(self.api_client)
+        return api.upload_checkpoint(body)
+
+    def search_checkpoint_pixels(self, project_id, checkpoint_id):
+        api = SearchApi(self.api_client)
+
+        checkpoint_id_condition: SearchRequestCondition = SearchRequestCondition()
+        checkpoint_id_condition.key = "checkpointId"
+        checkpoint_id_condition.operator = "="
+        checkpoint_id_condition.value = str(checkpoint_id)
+
+        project_id_condition: SearchRequestCondition = SearchRequestCondition()
+        project_id_condition.key = "Project.id"
+        project_id_condition.operator = "="
+        project_id_condition.value = str(project_id)
+
+        pagination: SearchRequestPagination = SearchRequestPagination()
+        pagination.page = 0
+        pagination.size = 1
+        pagination.sorts = []
+
+        search_request: SearchRequest = SearchRequest()
+        search_request.type = "CheckpointPixel"
+        search_request.conditions = [
+            checkpoint_id_condition,
+            project_id_condition
+        ]
+        search_request.pagination = pagination
+        return api.search(search_request)
--- testops_commons/uploader/uploader.py
+++ testops_commons/uploader/uploader.py
@@ -1,17 +1,12 @@
-import json
 import logging
 from os import path, getenv
 from pathlib import Path
 import time
 
-import testops_api
-from testops_api import ApiClient
 from testops_api.model.file_resource import FileResource
 from testops_api.model.upload_batch_file_resource import \
     UploadBatchFileResource
-from urllib3 import PoolManager, make_headers, HTTPConnectionPool, HTTPSConnectionPool
-import urllib3
-from urllib3.exceptions import InsecureRequestWarning
+from testops_api.model.upload_checkpoint_resource import UploadCheckpointResource
 
 from testops_commons.configuration.configuration import \
     Configuration
@@ -19,6 +14,7 @@ from testops_commons.core import constants
 from testops_commons.helper import file_helper, helper
 from testops_commons.model.models import Apis, CheckpointMatchStatus, CheckpointPixel, RequestMethod, TestOpsException, UploadInfo, VisualTestingCheckpointMismatchException, VisualTestingTimeoutException
 from testops_commons.testops_connector import TestOpsConnector
+from utils import create_api_client
 
 PROXY_PROTOCOL_HTTP = "http"
 
@@ -34,32 +30,7 @@ class TestOpsReportUploader(ReportUploader):
     def __init__(self, configuration: Configuration):
         self.configuration = configuration
         self.report_pattern = constants.REPORT_PATTERN
-        self.testops_connector = TestOpsConnector(self.create_api_client())
-
-    def create_api_client(self) -> ApiClient:
-        config: testops_api.Configuration = testops_api.Configuration(
-            host=self.configuration.server_url,
-            username='',
-            password=self.configuration.api_key
-        )
-        config.verify_ssl = False
-
-        if self.configuration.proxy_information.host:
-            if self.configuration.proxy_information.protocol == PROXY_PROTOCOL_HTTP:
-                config.proxy = HTTPConnectionPool(host=self.configuration.proxy_information.host
-                                                  , port=self.configuration.proxy_information.port)
-            elif self.configuration.proxy_information.protocol == PROXY_PROTOCOL_HTTPS:
-                config.proxy = HTTPSConnectionPool(host=self.configuration.proxy_information.host
-                                                   , port=self.configuration.proxy_information.port)
-            proxy_user = self.configuration.proxy_information.username
-            proxy_pass = self.configuration.proxy_information.password
-            if (proxy_user is not None) and (proxy_pass is not None):
-                config.proxy_headers = make_headers(proxy_basic_auth="{}:{}"
-                                                    .format(self.configuration.proxy_information.username,
-                                                            self.configuration.proxy_information.password))
-
-        client: ApiClient = ApiClient(config)
-        return client
+        self.testops_connector = TestOpsConnector(create_api_client(self))
 
     def upload_file(self, info: FileResource, file_path: str, is_end: bool) -> UploadBatchFileResource:
         file_resource: UploadBatchFileResource = UploadBatchFileResource()
@@ -103,68 +74,40 @@ class TestOpsReportUploader(ReportUploader):
 
 class VisualTestingUploader:
     
-    def __init__(self, timeout: int = 60) -> None:
-        urllib3.disable_warnings(InsecureRequestWarning) # Suppress SSL warning
-        self.timeout = timeout
-        self.__wait_time = 5
-        
-        self.project_id: int = int(getenv(constants.TESTOPS_PROJECT_ID_ENV))
-        self.api_key: str = getenv(constants.TESTOPS_API_KEY_ENV)
-        self.server: str = getenv(constants.TESTOPS_SERVER_URL_ENV)
-        self.session_id: str = getenv(constants.TESTOPS_SESSION_ID_ENV)
-
-        self.__api_headers = helper.get_api_auth_headers(self.api_key)
-        self.__http = PoolManager(1, cert_reqs="CERT_NONE")
+    def __init__(self, configuration: Configuration):
+        self.configuration = configuration
+        self.report_pattern = constants.REPORT_PATTERN
+        self.testops_connector = TestOpsConnector(create_api_client(self))
         self.__logger = logging.getLogger(__name__)
+        self.project_id: int = int(getenv(constants.TESTOPS_PROJECT_ID_ENV))
+        self.session_id: str = getenv(constants.TESTOPS_SESSION_ID_ENV)
+        
 
+    def __get_upload_info(self) -> FileResource:
+        api_key = self.configuration.api_key
+        if helper.is_blank(api_key):
+            return None
 
-    def __get_upload_info(self) -> UploadInfo:
+        project_id = self.configuration.project_id
+        if project_id is None:
+            return None
         try:
             self.__logger.info("Connecting to Katalon TestOps")
-            r = self.__http.request(
-                RequestMethod.GET,
-                self.server + Apis.GET_UPLOAD_URL,
-                fields={"projectId": str(self.project_id)},
-                headers=self.__api_headers,
-            )
-            info = json.loads(r.data.decode())
-            return UploadInfo(info["path"], info["uploadUrl"])
+            return self.testops_connector.get_upload_url(project_id=project_id)
         except Exception as e:
             self.__logger.error(__name__ + " Error.")
             raise TestOpsException(e)
 
 
-    def __upload_file_s3(self, upload_url: str, image_path: str, data: bytes) -> None:
-        try:
-            self.__logger.info("Uploading Checkpoint image %s to Katalon TestOps", image_path)
-            self.__http.request(
-                RequestMethod.PUT,
-                upload_url,
-                body=data,
-                headers={"Content-Type": "image/*"},
-            )
-        except Exception as e:
-            self.__logger.error("Failed to upload Checkpoint image %s", image_path)
-            raise TestOpsException(e)
-
-
     def __send_vst_info(self, name: str, path: str) -> int:
         try:
-            r = self.__http.request(
-                RequestMethod.POST,
-                self.server + Apis.UPLOAD_CHECKPOINT,
-                body=json.dumps(
-                    {
-                        "projectId": str(self.project_id),
-                        "sessionId": self.session_id,
-                        "batch": helper.generate_upload_batch(),
-                        "fileName": name,
-                        "uploadedPath": path,
-                    }
-                ),
-                headers=self.__api_headers,
-            )
-            return int(json.loads(r.data.decode())["id"])
+            upload_checkpoint_resource: UploadCheckpointResource = UploadCheckpointResource()
+            upload_checkpoint_resource.project_id = str(self.project_id)
+            upload_checkpoint_resource.session_id = self.session_id
+            upload_checkpoint_resource.batch = helper.generate_upload_batch()
+            upload_checkpoint_resource.file_name = name
+            upload_checkpoint_resource.uploaded_path = path
+            return self.testops_connector.upload_checkpoint(upload_checkpoint_resource).id
         except Exception as e:
             self.__logger.error(__name__ + " Error.")
             raise TestOpsException(e)
@@ -172,30 +115,8 @@ class VisualTestingUploader:
 
     def __get_vst_result(self, checkpoint_id: int) -> CheckpointPixel:
         try:
-            r = self.__http.request(
-                RequestMethod.POST,
-                self.server + Apis.SEARCH,
-                body=json.dumps(
-                    {
-                        "pagination": {"page": 0, "size": 1, "sorts": []},
-                        "conditions": [
-                            {
-                                "key": "checkpointId",
-                                "operator": "=",
-                                "value": str(checkpoint_id),
-                            },
-                            {
-                                "key": "Project.id",
-                                "operator": "=",
-                                "value": str(self.project_id),
-                            },
-                        ],
-                        "type": "CheckpointPixel",
-                    }
-                ),
-                headers=self.__api_headers,
-            )
-            checkpoint_results: list = json.loads(r.data.decode()).get("content", [])
+            checkpoint_pixels_search_result = self.testops_connector.search_checkpoint_pixels(project_id=self.project_id, checkpoint_id=checkpoint_id)
+            checkpoint_results: list = checkpoint_pixels_search_result.content
             if len(checkpoint_results) == 0:
                 return CheckpointPixel(None, None)
 
@@ -214,9 +135,7 @@ class VisualTestingUploader:
         upload_info = self.__get_upload_info()
 
         # Upload file
-        with open(image_path, "rb") as f:
-            image_data = f.read()
-            self.__upload_file_s3(upload_info.upload_url, image_path, image_data)
+        self.testops_connector.upload_file(upload_info.upload_url, image_path)
 
         checkpoint_name = file_helper.get_file_name(image_path)
         checkpoint_id: int = self.__send_vst_info(checkpoint_name, upload_info.path)
